17d9bace6f77f3083d339afab9e36aa9262d5b1e
rikasanai <chensiyi@bao.tv>
2016-03-21 08:43:03 +0000

    async

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
-# source 'http://baozou:baozouqwer@gems.baozou.com/'
 source 'https://ruby.taobao.org/'
 
 gem 'sinatra'
 gem 'pry'
 gem 'pry-byebug'
 gem 'byebug'
+gem 'concurrent-ruby', require: 'concurrent'
--- github_webhook.rb
+++ github_webhook.rb
@@ -1,10 +1,38 @@
 require 'sinatra'
 require 'json'
 require 'open3'
+require 'concurrent'
+
+class ShellRunner
+  include Concurrent::Async
+
+  def mvn_build
+    project = "palinuridae"
+    path = "/opt/deploy/#{project}"
+    # git pull
+    cmds = %Q{
+      git remote update -p
+      git checkout -f origin/master
+      git submodule update --init
+      mvn clean install
+    }
+    cmds.lines.map(&:strip).delete_if(&:empty?).each do |cmd|
+      p "RUN: >>#{cmd}<<"
+      oe, ts = Open3.capture2e cmd,chdir: path
+      p "STATUS: #{ts.exitstatus}"
+      p "===============stdout_and_stderr============"
+      print oe
+      p ""
+      p ""
+    end
+  end
+end
+
 
 configure do
   set :bind, '0.0.0.0'
   set :port, 9292
+  File.open('sinatra.pid', 'w') {|f| f.write Process.pid }
 end
 
 get '/' do
@@ -15,28 +43,6 @@ post '/payload' do
   request.body.rewind
   payload = JSON.parse request.body.read
   if request.env['HTTP_X_GITHUB_EVENT'] == 'push'
-    mvn_build if payload['ref'] == 'refs/heads/master'
-  end
-end
-
-def mvn_build
-  # project = "docker-controller"
-  project = "palinuridae"
-  path = "/opt/deploy/#{project}"
-  # git pull
-  cmds = %Q{
-    git remote update -p
-    git checkout -f origin/master
-    git submodule update --init
-    mvn clean install
-  }
-  cmds.lines.map(&:strip).delete_if(&:empty?).each do |cmd|
-    p "RUN: >>#{cmd}<<"
-    oe, ts = Open3.capture2e cmd,chdir: path
-    p "STATUS: #{ts.exitstatus}"
-    p "===============stdout_and_stderr============"
-    print oe
-    p
-    p
+    ShellRunner.new.async.mvn_build if payload['ref'] == 'refs/heads/master'
   end
 end
